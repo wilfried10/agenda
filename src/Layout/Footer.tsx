dc1b51c0c905c39786f5cc29d71337540f540c4d
Wilfried <contactwilfriedfotie@gmail.com>
2022-11-18 21:05:49 +0100

    File structure and utility components

---
+++ src/Layout/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='min-h-[10vw] mt-10 space-y-2 flex flex-col items-center justify-center'>
+
+
+            <a href="">Version 0.0.0</a>
+        </div>
+    )
+}
+
+export default Footer
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import ErrorBoundary from './components/ErrorBoundary'
+import { Loader } from './components/Loader'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Suspense fallback={<Loader />}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </BrowserRouter>
+    </React.StrictMode>
+  </Suspense>
 )
